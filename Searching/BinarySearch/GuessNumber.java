114d7a7d661fbd588797659d8f5488738f55db3c
Mohammed Arshath <sm.arshath2003@gmail.com>
2024-10-28 22:18:38 +0530

    Binary search problem

---
+++ Searching/BinarySearch/GuessNumber.java
@@ -0,0 +1,50 @@
+/*
+We are playing the Guess Game. The game is as follows:
+
+I pick a number from 1 to n. You have to guess which number I picked.
+
+Every time you guess wrong, I will tell you whether the number I picked is higher or lower than your guess.
+
+You call a pre-defined API int guess(int num), which returns three possible results:
+
+-1: Your guess is higher than the number I picked (i.e. num > pick).
+1: Your guess is lower than the number I picked (i.e. num < pick).
+0: your guess is equal to the number I picked (i.e. num == pick).
+Return the number that I picked.
+
+
+ */
+
+package Searching.BinarySearch;
+
+public class GuessNumber {
+    public int guess(int num) {
+        // Dummy implementation for example
+        // Replace with actual logic to return -1, 1, or 0 based on the picked number
+        return 0;
+    }
+
+    public int guessNumber(int n) {
+        int left = 1;
+        int right = n;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            int resp = guess(mid);
+            if (resp == 0) {
+                return mid;
+            } else if (resp < 0) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+    public static void main(String[] args) {
+        GuessNumber guessNumber = new GuessNumber();
+        int n = 10;
+        int result = guessNumber.guessNumber(n);
+        System.out.println("The guessed number is: " + result);
+    }
+}
