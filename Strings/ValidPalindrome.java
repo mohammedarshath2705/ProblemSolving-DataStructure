f7f0100cb5189959d4ef742c160c3401e0f66f94
Mohammed Arshath <sm.arshath2003@gmail.com>
2024-10-30 22:33:43 +0530

    String problem

---
+++ Strings/ValidPalindrome.java
@@ -0,0 +1,29 @@
+/*
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+ */
+package Strings;
+
+public class ValidPalindrome {
+
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+
+        int l = 0;
+        int r = s.length() - 1;
+
+        while (l < r) {
+            if (s.charAt(l++) != s.charAt(r--)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        ValidPalindrome vp = new ValidPalindrome();
+        System.out.println(vp.isPalindrome("A man, a plan, a canal: Panama")); // true
+        System.out.println(vp.isPalindrome("race a car")); // false
+    }
+}
