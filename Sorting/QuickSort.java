a2debfdeb71b543ce2edd3797fc0b05f92f3815b
Mohammed Arshath <sm.arshath2003@gmail.com>
2024-11-06 22:57:02 +0530

    Sorting problem

---
+++ Sorting/QuickSort.java
@@ -0,0 +1,40 @@
+package Sorting;
+
+public class QuickSort {
+
+    static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+        for (int j = low; j <= high - 1; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i + 1, high);
+        return i + 1;
+    }
+
+    static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {100,90,80,70,60,50,10,20,30,40};
+        int n = arr.length;
+        quickSort(arr, 0, n - 1);
+        for (int val : arr) {
+            System.out.print(val + " ");
+        }
+    }
+}
