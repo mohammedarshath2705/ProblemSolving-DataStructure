58546a79f5ac979617960a9f103434e803c665e1
Mohammed Arshath <sm.arshath2003@gmail.com>
2024-12-19 19:56:22 +0530

    List problem

---
+++ LinkedList/RotateList.java
@@ -0,0 +1,47 @@
+package LinkedList;
+
+public class RotateList {
+    public ListNode rotateRight(ListNode head, int k) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode cur = head;
+        int n = 0;
+        for (; cur != null; cur = cur.next) {
+            n++;
+        }
+        k %= n;
+        if (k == 0) {
+            return head;
+        }
+        ListNode fast = head;
+        ListNode slow = head;
+        while (k-- > 0) {
+            fast = fast.next;
+        }
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        ListNode ans = slow.next;
+        slow.next = null;
+        fast.next = head;
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        RotateList rotateList = new RotateList();
+        ListNode result = rotateList.rotateRight(head, 2);
+
+        while (result != null) {
+            System.out.print(result.val + " ");
+            result = result.next;
+        }
+    }
+}
