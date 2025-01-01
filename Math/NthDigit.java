31ea45d494523f5c6eb0258bca478001de74dbc9
Mohammed Arshath <sm.arshath2003@gmail.com>
2025-01-01 19:17:50 +0530

    Math problem

---
+++ Math/NthDigit.java
@@ -0,0 +1,30 @@
+package Math;
+
+public class NthDigit {
+    public int findNthDigit(int n) {
+        int len = 1;
+        long count = 9;
+        int start = 1;
+
+        while (n > len * count) {
+            n -= len * count;
+            len += 1;
+            count *= 10;
+            start *= 10;
+        }
+
+        start += (n - 1) / len;
+        String s = Integer.toString(start);
+        return Character.getNumericValue(s.charAt((n - 1) % len));
+    }
+
+    public static void main(String[] args) {
+        NthDigit solution = new NthDigit();
+
+        System.out.println(solution.findNthDigit(3)); // 3
+        System.out.println(solution.findNthDigit(11)); // 0
+        System.out.println(solution.findNthDigit(15)); // 2
+        System.out.println(solution.findNthDigit(19)); // 4
+        System.out.println(solution.findNthDigit(1000)); // 3
+    }
+}
