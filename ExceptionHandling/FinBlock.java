3107517b537217aa5e13bfb8c2466f31974d5c93
mohammedarshath2705 <sm.arshath2003@gmail.com>
2025-10-13 11:28:59 +0530

    Exception problem

---
+++ ExceptionHandling/FinBlock.java
@@ -0,0 +1,13 @@
+package ExceptionHandling;
+
+public class FinBlock {
+    public static void main(String[] args) {
+        try {
+            int x = 10 / 0;
+        } catch (ArithmeticException e) {
+            System.out.println("Caught: " + e.getMessage());
+        } finally {
+            System.out.println("Finally block always runs.");
+        }
+    }
+}
