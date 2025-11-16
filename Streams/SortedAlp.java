34c69e47c0f69a76d8f3ed22eab738f539d55e72
mohammedarshath2705 <sm.arshath2003@gmail.com>
2025-11-16 05:42:03 +0530

    Stream problem

--- Streams/SortedAlp.java
+++ Streams/SortedAlp.java
@@ -11,6 +11,11 @@ public class SortedAlp {
                 .limit(3)
                 .toList();
 
+        List<String> result = names.stream()
+                        .filter(n->n.startsWith("A"))
+                .toList();
+
         System.out.println("First 3 Sorted Names: " + firstThree);
+        System.out.println("Names starts with A: " + result);
     }
 }
