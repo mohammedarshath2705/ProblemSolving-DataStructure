11bc8d26967a8a009d6173149dae90cc74d7509e
mohammedarshath2705 <sm.arshath2003@gmail.com>
2025-11-22 11:49:40 +0530

    Stream problem

--- Streams/EvenNumber.java
+++ Streams/EvenNumber.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class EvenNumber {
     public static void main(String[] args) {
-        List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+        List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,-100,8,9,10);
 
         List<Integer> even = numbers.stream()
                 .filter(n -> n % 2 == 0)
@@ -17,7 +17,14 @@ public class EvenNumber {
                 .findFirst()
                 .orElse(-1);
 
+        int min = numbers.stream()
+                .min(Integer::compare)
+                .orElse(-1);
+
+
         System.out.println("Even Numbers: " + even);
         System.out.println("Second Highest Number : " + secondHighest);
+        System.out.println("Minimum Number : " + min);
+
     }
 }
